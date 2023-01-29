2684916652561169418885c3d5d71679f7a801fd
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-01-29 09:45:58 +0200

    modified the code

--- labs/unit2/unit2_lesson4_array_and_string/lab1.c
+++ labs/unit2/unit2_lesson4_array_and_string/lab1.c
@@ -10,9 +10,14 @@ int main()
     /*  scan 10 student degrees     */
     int i;
     for (i=0; i<10; i++)
+    {
+        printf("Enter student %d degree: ",i+1);
         scanf("%f",&degree[i]);
+    }
+    
+    printf("\n\n");
 
     /*  print the 10 student degrees */
     for (i=0; i<10; i++)
-        printf("%.2f\n",degree[i]);
+        printf("Student %d degree is %.2f\n",i+1, degree[i]);
 }
