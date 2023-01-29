1c3d6492f986fa285733e61570b73c2d32a56766
AdhamMahgoub <120437302+AdhamMahgoub@users.noreply.github.com>
2023-01-29 09:27:00 +0200

    adding comment

--- assignments/unit_2/unit2_lesson3_C_Basics/homework2/EX2_vowel_or_consonent.c (using_ternary_function).c
+++ assignments/unit_2/unit2_lesson3_C_Basics/homework2/EX2_vowel_or_consonent.c (using_ternary_function).c
@@ -11,5 +11,6 @@ int main()
     printf("Enter an alphabet: ");
     scanf("%c",&x);
 
+    /*  checking if a vowel or consonent using ternary operator  */
     (x=='a' || x=='e' || x=='i' || x=='o')? printf("%c is a vowel" ,x):printf("%c is a consonant" ,x);
 }
