08e5d1ae860964fb5baf4b043414e7a1a2c88374
AdhamMahgoub <120437302+AdhamMahgoub@users.noreply.github.com>
2023-09-09 18:14:04 +0300

    modified the code

--- unit_2_C/assignments/unit2_lesson7_pointers/Q2.c
+++ unit_2_C/assignments/unit2_lesson7_pointers/Q2.c
@@ -3,20 +3,34 @@
 
 int main()
 {
-
 	int arr[26];
-
 	int i;
 	for (i=0; i<26; i++)
 		arr[i] = 65+i;
-
 	int* ptr = arr;
 	for (i=0; i<26; i++)
 	{
 		printf("%c ",*(ptr+i));
 	}
-
-
-
  	return 0;
 }
+////////////////////////////////////////////////////////Version 2////////////////////////////////////////////////////////
+#include <stdio.h>
+
+
+int main()
+{
+
+	char arr[26];
+	int i;
+	char* ptr = arr;
+	for (i=0; i<26; i++)
+		*(ptr+i) = 'A'+i;
+
+	for (i=0; i<26; i++)
+	{
+		printf("%c ",*(ptr+i));
+	}
+ 	return 0;
+}
+
