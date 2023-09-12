0c7ba811b34c58de763814b889d08528c3b9704b
user1 <adhammahgoub1@gmail.com>
2023-09-12 11:25:16 +0300

    modified the Platform_Types.h

--- unit_3_embedded_c/Lesson1/Assignment/Platform_Types.h
+++ unit_3_embedded_c/Lesson1/Assignment/Platform_Types.h
@@ -1,13 +1,33 @@
-typedef unsigned 				char 	u8; 
-typedef unsigned 	short 		int  	u16; 
-typedef unsigned 				int  	u32; 
-typedef unsigned 	long		int  	u64; 
+#ifndef PLATFORM_TYPES_H
+#define PLATFORM_TYPES_H_
 
-typedef signed 					char 	s8; 
-typedef signed 		short 		int  	s16; 
-typedef signed 					int  	s32; 
-typedef signed 		long		int  	s64; 
+typedef _Bool                       		boolean;
 
-typedef 						float  	f32; 
-typedef  						double 	f64; 
-typedef 			long 		double 	f128; 
+typedef signed char                 		sint8_t;		//	1 byte 
+typedef unsigned char               		uint8_t;
+
+typedef signed short                		sint16_t;		//	2 bytes
+typedef unsigned short              		uint16_t;
+
+typedef signed int                  		sint32_t;		// 	4 bytes
+typedef unsigned int                		uint32_t;
+
+typedef signed long long int        		sint64_t;		//	8 bytes 
+typedef unsigned long long int      		uint64_t;
+
+typedef volatile char                       vchar_t;
+typedef volatile signed char                vsint8_t;
+typedef volatile unsigned char              vuint8_t;
+
+typedef volatile signed short               vsint16_t;
+typedef volatile unsigned short             vuint16_t;
+
+typedef volatile signed int                 vsint32_t;
+typedef volatile unsigned int               vuint32_t;
+typedef volatile signed long long int       vsint64_t;
+typedef volatile unsigned long long int     vuint64_t;
+
+typedef float       						float32;
+typedef double      						float64;
+
+#endif 
