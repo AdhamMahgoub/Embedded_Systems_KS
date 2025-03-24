43a7d602769ea940bddca37f26a3f0d991784ada
AdhamA <adma00003@stud.uni-saarland.de>
2025-03-25 01:49:31 +0200

    implemented the startup.c -- copying not implemented yet

---
+++ unit_3_embedded_c/Lesson3/Lab2_STM32_Blink/Lab_2b_startup_with_copying/startup.c
@@ -0,0 +1,33 @@
+#include <stdint.h>
+
+#define STACK_Start_SP  0x20001000
+
+extern int main(void);
+
+void Rest_Handler(void);
+
+void Default_Handler(void)
+{
+    Rest_Handler();
+}
+
+void NMI_Handler(void)           __attribute__((weak, alias("Default_Handler")));
+void H_fault_Handler(void)       __attribute__((weak, alias("Default_Handler")));
+void MM_Fault_Handler(void)      __attribute__((weak, alias("Default_Handler")));
+void Bus_Fault(void)             __attribute__((weak, alias("Default_Handler")));
+void Usage_Fault_Handler(void)   __attribute__((weak, alias("Default_Handler")));
+
+uint32_t vectors[] __attribute__((section(".vectors"))) = {
+    STACK_Start_SP,
+    (uint32_t) &Rest_Handler,
+    (uint32_t) &NMI_Handler,
+    (uint32_t) &H_fault_Handler,
+    (uint32_t) &MM_Fault_Handler,
+    (uint32_t) &Bus_Fault,
+    (uint32_t) &Usage_Fault_Handler
+};
+
+void Rest_Handler(void)
+{
+    main();
+}
