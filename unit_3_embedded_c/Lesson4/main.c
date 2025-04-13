80f196037c6706350cd4692537a1933c800547a0
AdhamA <adma00003@stud.uni-saarland.de>
2025-04-13 13:43:09 +0200

    final working version

--- unit_3_embedded_c/Lesson4/main.c
+++ unit_3_embedded_c/Lesson4/main.c
@@ -1,5 +1,5 @@
 #define SYSCTL_RCGC2_R       (*((volatile unsigned long *)0x400FE108))
-// #define GPIO_PORTF_DATA_R    (*((volatile unsigned long *)0x400253FC))
+#define GPIO_PORTF_DATA_R    (*((volatile unsigned long *)0x400253FC))
 #define GPIO_PORTF_DIR_R     (*((volatile unsigned long *)0x40025400))
 #define GPIO_PORTF_DEN_R     (*((volatile unsigned long *)0x4002551C))
 
@@ -7,15 +7,8 @@ int global_initialized_data = 5;
 double global_uninitialized_bss; 
 
 
-volatile unsigned long* GPIO_PORTF_DATA_R = (volatile unsigned long *)0x400253FC;
-
-
 int main(void)
 {
-
-
-
-	unsigned long arr_disrupt [245]; // stored in stack
     volatile unsigned long delay_count;
     SYSCTL_RCGC2_R = 0x00000020;               // Enable clock to Port F
     for (delay_count = 0; delay_count < 200; delay_count++); // Delay for clock to stabilize
@@ -25,18 +18,12 @@ int main(void)
 
     while (1)
     {
-        *GPIO_PORTF_DATA_R |= 1 << 3;           // Set PF3
+        GPIO_PORTF_DATA_R |= 1 << 3;           // Set PF3
         for (delay_count = 0; delay_count < 200000; delay_count++); // Delay
 
-        *GPIO_PORTF_DATA_R &= ~(1 << 3);        // Clear PF3
+        GPIO_PORTF_DATA_R &= ~(1 << 3);        // Clear PF3
         for (delay_count = 0; delay_count < 200000; delay_count++); // Delay
-		
-		for (volatile int i = 0; i < 245; i++) 
-		{
-			arr_disrupt[i] = i * 10; // or any other logic you want
-		}
-	
-	}
+    }
 
     return 0;
 }
